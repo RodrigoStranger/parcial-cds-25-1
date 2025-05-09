429e2ccd162e43b0606d201d8c2b3f2f53b18a39
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-05-08 23:05:16 -0500

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const pool = require('./src/config/database');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3500;
 
 (async () => {
   try {
