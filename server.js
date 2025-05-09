37f317c23bd68f28a0f85d28f436668e06f53d82
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-05-09 17:44:28 -0500

    delete

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const productosRouter = require('./src/routes/productos.routes');
 const telefonosRouter = require('./src/routes/telefonos_personas.routes');
 const telefonosProveedoresRouter = require('./src/routes/telefonos_proveedores.routes')
 const direccionesPersonasRouter = require('./src/routes/direcciones_personas.routes')
-const personasRouter = require('./src/routes/personas.routes')
 
 const app = express();
 
@@ -21,7 +20,6 @@ app.use('/productos', productosRouter);
 app.use('/telefonos_personas', telefonosRouter);
 app.use('/telefonos_proveedores', telefonosProveedoresRouter);
 app.use('/direcciones_personas', direccionesPersonasRouter);
-app.use('/personas', personasRouter);
 
 const PORT = process.env.PORT || 3000;
 
